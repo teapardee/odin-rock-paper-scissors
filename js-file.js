aebe50ca4056497645f4d3038818805cabc44f96
Tyler Pardee <tjpardee@gmail.com>
2022-05-25 17:24:17 -0400

    Add Winner / Loser Prompt & Score Reset

--- js-file.js
+++ js-file.js
@@ -1,3 +1,8 @@
+/* Declare global variables for keeping score (revisit? look into a way to accomplish this without global variables?) */
+
+var computerScore = 0;
+var playerScore = 0;
+
 /* Event Listeners */
 
 const actions = document.querySelectorAll(".action");
@@ -8,73 +13,6 @@ actions.forEach((action) => {
   });
 });
 
-/* Randomly select whether computer plays rock, paper or scissors */
-
-function computerPlay() {
-  let randomNum = Math.random();
-
-  switch (true) {
-    case randomNum <= 0.33:
-      return "rock";
-      break;
-    case randomNum >= 0.66:
-      return "paper";
-      break;
-    default:
-      return "scissors";
-  }
-}
-
-/* Play round of RPS and determine winner */
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    console.log("It's a tie! Play again!");
-    return "neither";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")
-  ) {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
-    return "player";
-  } else if (
-    (playerSelection === "scissors" && computerSelection === "rock") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "rock" && computerSelection === "paper")
-  ) {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}!`);
-    return "computer";
-  }
-}
-
-/* Declare global variables for keeping score (revisit? look into a way to accomplish this without global variables?) */
-
-var computerScore = 0;
-var playerScore = 0;
-
-/* Count score and display record at the end of the game */
-
-function scoreCounter(gameOutcome) {
-  if (gameOutcome === "player") {
-    window.playerScore++;
-  } else if (gameOutcome === "computer") {
-    window.computerScore++;
-  }
-}
-
-function displayRecord() {
-  const score = document.querySelector(".score");
-
-  score.textContent = `${window.playerScore} : ${window.computerScore}`;
-
-  if (window.computerScore > window.playerScore) {
-    return `Computer Wins ${window.computerScore} : ${window.playerScore} !`;
-  } else {
-    return `Player Wins ${window.playerScore} : ${window.computerScore} !`;
-  }
-}
-
 /* Play RPS with computer 5 times */
 
 function game(userInput) {
@@ -83,5 +21,78 @@ function game(userInput) {
   let gameOutcome = playRound(playerSelection, computerSelection);
   let record = scoreCounter(gameOutcome);
 
-  console.log(displayRecord());
+  displayRecord();
+
+  /* Randomly select whether computer plays rock, paper or scissors */
+
+  function computerPlay() {
+    let randomNum = Math.random();
+
+    switch (true) {
+      case randomNum <= 0.33:
+        return "rock";
+        break;
+      case randomNum >= 0.66:
+        return "paper";
+        break;
+      default:
+        return "scissors";
+    }
+  }
+
+  /* Play round of RPS and determine winner */
+
+  function playRound(playerSelection, computerSelection) {
+    const outcome = document.querySelector("#outcome");
+
+    if (playerSelection === computerSelection) {
+      outcome.textContent = "It's a tie! Play again!";
+      console.log("It's a tie! Play again!");
+      return "neither";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+      outcome.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
+      return "player";
+    } else if (
+      (playerSelection === "scissors" && computerSelection === "rock") ||
+      (playerSelection === "paper" && computerSelection === "scissors") ||
+      (playerSelection === "rock" && computerSelection === "paper")
+    ) {
+      outcome.textContent = `You lose! ${computerSelection} beats ${playerSelection}!`;
+
+      return "computer";
+    }
+  }
+
+  /* Count score and display record at the end of the game */
+
+  function scoreCounter(gameOutcome) {
+    if (gameOutcome === "player") {
+      window.playerScore++;
+    } else if (gameOutcome === "computer") {
+      window.computerScore++;
+    }
+  }
+
+  function displayRecord() {
+    const displayScore = document.querySelector(".score");
+    console.log(window.playerScore);
+    if (window.playerScore === 5) {
+      displayScore.textContent = `YOU WIN! ${window.playerScore} : ${window.computerScore} !`;
+      displayScore.style.color = "rgb(27, 163, 156)";
+      window.playerScore = 0;
+      window.computerScore = 0;
+    } else if (window.computerScore === 5) {
+      displayScore.textContent = `Computer Wins ${window.computerScore} : ${window.playerScore} !`;
+      displayScore.style.color = "rgb(296, 77, 86)";
+      window.playerScore = 0;
+      window.computerScore = 0;
+    } else {
+      displayScore.textContent = `${window.playerScore} : ${window.computerScore}`;
+      displayScore.style.color = "white";
+    }
+  }
 }
